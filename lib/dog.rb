63b6a148c726a2d9b07202fc5d0c55199b550312
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-24 12:21:02 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,62 @@
+class Dog
+  attr_accessor :breed, :name, :id
+
+  def initialize(breed:, name:, id: nil)
+    @breed = breed
+    @name = name
+    @id = id
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS DOGS"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+       INSERT INTO dogs (name, breed)
+       VALUES (?, ?)
+      SQL
+
+   DB[:conn].execute(sql, self.name, self.breed)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+   self
+ end
+
+ def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+end
